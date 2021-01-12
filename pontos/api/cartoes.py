520ca1fa212db6e692843ed0f586502b35d6f77e
Roger Camargo <huogerac@gmail.com>
2021-01-12 15:56:44 -0200

    Adiciona API de pontos

--- pontos/api/cartoes.py
+++ pontos/api/cartoes.py
@@ -6,3 +6,11 @@ def listar_cartoes():
     cartoes = cartoes_services.listar_cartoes()
 
     return {"data": cartoes}
+
+
+def adiciona_ponto(cartao_id):
+    return {}
+
+
+def remove_ponto(cartao_id):
+    return {}
