7052cf1bfbd2f52472ef7ea310f67c77c3c90e57
Roger Camargo <huogerac@gmail.com>
2021-01-15 14:57:08 -0200

    Permite acesso api sem token (tirar comentarios do yaml para protecao)

--- pontos/api/cartoes.py
+++ pontos/api/cartoes.py
@@ -6,9 +6,9 @@ def listar_cartoes(empresa_id=None, programa_id=None, page=1, page_size=500, tok
     return {"data": cartoes}
 
 
-def adicionar_ponto(cartao_id, token_info):
+def adicionar_ponto(cartao_id, token_info=None):
     return cartoes_services.adicionar_ponto(cartao_id)
 
 
-def remover_ponto(cartao_id, token_info):
+def remover_ponto(cartao_id, token_info=None):
     return cartoes_services.remover_ponto(cartao_id)
